8cb385a8a5a91234a3d1e6aefdc8934a6fd8585b
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-07-02 12:23:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+// index.js
+
+// Defines saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Defines mondayWork function expression
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Defines wrapAdjective function
+function wrapAdjective(wrapper = '*') {
+    return function (adjective = 'special') {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
+
